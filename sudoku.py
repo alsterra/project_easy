d4cd4147ab5dbb45537ec135e81e8190d6ec5b30
Alsterra <64342976+alsterra@users.noreply.github.com>
2022-01-01 08:07:45 +0700

    cahnge in sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,2 +1,11 @@
+def find_next_empty(puzzle):
+
+
 def solve_sudoku(puzzle):
-    pass
+    # solve sudoku using backtracking!
+    # our puzzle is a list of lists, where each inner list is a row in our sudoku puzzle
+    # return wether a solution exists
+    # mutates puzzle to be the solution (if solution exists)
+
+    # step 1: choose somewhere on the puzzle to make a guess
+    row, col = find_next_empty(puzzle)
